3eda269c074ed481641bc845c8f4aad9847928a7
Andrew Olson <olson@cshl.edu>
2021-09-29 11:47:49 -0400

    remove rice from dropdown menu of sites

--- conf/index.js
+++ conf/index.js
@@ -4,5 +4,5 @@ import grapevine from './grapevine.json';
 import rice from './rice.json';
 import sorghum from './sorghum.json';
 
-export default [main,maize,grapevine,rice,sorghum];
+export default [main,maize,grapevine,sorghum];
 
