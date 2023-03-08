d5bde375bfa296c74fa57c91f88ef090143635de
Andrew Olson <olson@cshl.edu>
2023-03-07 22:27:47 -0500

    script to build all the sites

---
+++ build_all.sh
@@ -0,0 +1,6 @@
+npm run tidy && node build.js main v65 live
+npm run tidy && node build.js grapevine v3 live
+npm run tidy && node build.js maize v2 live
+npm run tidy && node build.js oryza v4 live
+npm run tidy && node build.js sorghum v3 live
+npm run tidy && node build.js yeast v1 live
